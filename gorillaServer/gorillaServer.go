0384510918f97ba48f47a1914d5772fe0538557d
Sam Watson <sam.watson@bowerswilkins.com>
2017-03-09 10:53:48 -0800

    gorillamux server to demonstrate URL building and URL path vars

---
+++ gorillaServer/gorillaServer.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func printPathVar(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprintf(w, "URL var is: %v\n", vars["someVar"])
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/path-var/{someVar}", printPathVar).Name("path")
+	builtUrl, _ := r.Get("path").URL("someVar", "foo")
+	fmt.Printf("Sample built URL: %s", builtUrl.String())
+	http.ListenAndServe(":8000", r)
+}
